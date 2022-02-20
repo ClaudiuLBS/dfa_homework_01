35c766736ab69107285e5ce58cea99dda73d32d2
Diana Clinciu <clinciu.diana@gmail.com>
2022-02-20 21:18:49 +0200

    commit 4

--- dfa_parser_engine.py
+++ dfa_parser_engine.py
@@ -1,4 +1,4 @@
-input_file = open('dfa_config_file', 'r')
+import sys
 
 sigma = []
 states = []
@@ -9,44 +9,44 @@ transitions = {}
 def clean_string(input_string:str):
   return input_string.replace('\n', '').replace(' ', '')
 
-
-line = None
-while line != '':
-  line = input_file.readline()
-  if 'sigma' in line.lower():
+def parse_config(file_name):
+  input_file = open(file_name, 'r')
+  line = None
+  while line != '':
     line = input_file.readline()
-    while line.strip().lower() != 'end':
-      sigma.append(clean_string(line))
+    if 'sigma' in line.lower():
       line = input_file.readline()
-
-  if 'states' in line.lower():
-    line = input_file.readline()
-    while line.strip().lower() != 'end':
-      line = clean_string(line)
-      state_tokens = line.split(',')
-      state_tokens[0] = clean_string(state_tokens[0])
-      states.append(state_tokens[0])
-
-      if len(state_tokens) > 1:
-        if 'F' in state_tokens:
-          final_states.append(state_tokens[0])
-          # print(state_tokens)
-        if 'S' in state_tokens:
-          initial_states.append(state_tokens[0])
+      while line.strip().lower() != 'end':
+        sigma.append(clean_string(line))
+        line = input_file.readline()
+  
+    if 'states' in line.lower():
       line = input_file.readline()
+      while line.strip().lower() != 'end':
+        line = clean_string(line)
+        state_tokens = line.split(',')
+        state_tokens[0] = clean_string(state_tokens[0])
+        states.append(state_tokens[0])
 
-  if 'transitions' in line.lower():
-    line = input_file.readline()
-    while line.strip().lower() != 'end':
-      line = clean_string(line)
-      [state1, road, state2] = line.split(',')
+        if len(state_tokens) > 1:
+          if 'F' in state_tokens:
+            final_states.append(state_tokens[0])
+          if 'S' in state_tokens:
+            initial_states.append(state_tokens[0])
+        line = input_file.readline()
 
-      if state1 in transitions:
-        transitions[state1].append((state2, road))
-      else:
-        transitions[state1] = [(state2, road)]
+    if 'transitions' in line.lower():
       line = input_file.readline()
+      while line.strip().lower() != 'end':
+        line = clean_string(line)
+        [state1, road, state2] = line.split(',')
 
+        if state1 in transitions:
+          transitions[state1].append((state2, road))
+        else:
+          transitions[state1] = [(state2, road)]
+        line = input_file.readline()
+  input_file.close()
 
 def pretty_print(input_matrix:dict):
   for key in input_matrix.keys():
@@ -54,6 +54,22 @@ def pretty_print(input_matrix:dict):
     for item in input_matrix[key]:
       print(f'({item[0]}, {item[1]}) ', end='')
     print()
+    
+def validate_dfa(sigma, states, transitions, initial_states, final_states):
+  if len(initial_states) != 1 or len(final_states)==0:
+    return False
+  for start_state in transitions.keys():
+    if start_state not in states:
+      return False
+    letter_freq={}
+    for end_state,transition_letter in transitions[start_state]:
+      letter_freq[transition_letter]=letter_freq.get(transition_letter,0)+1
+      if letter_freq[transition_letter]>1:
+        return False
+      if transition_letter not in sigma:
+        return False
+      if end_state not in states:
+        return False
+  return True
 
-pretty_print(transitions)
-# print(sigma, states, initial_states, final_states ,sep='\n')
+parse_config(sys.argv[1])
